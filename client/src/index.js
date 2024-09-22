abd1152af453809af2aad094c649468e6df019c7
fafnir <gokurohono@gmail.com>
2024-09-22 14:51:54 +1000

    feat: initial cytoscape code

--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,35 @@
 
-console.log("PWEND")
+const initCY = () => {
+  const cy = cytoscape({
+    container: document.getElementById("cy"), 
+    elements: [
+      { data: { id: 'a' }, position: { x: 100, y: 100 } },
+      { data: { id: 'b' }, position: { x: 200, y: 200 } },
+      { data: { id: 'ab', source: 'a', target: 'b' } },
+    ],
+    style: [
+      {
+        selector: 'node',
+        style: {
+          'background-color': '#666',
+          'label': 'data(id)',
+        }
+      },
+      {
+        selector: 'edge',
+        style: {
+          'width': 3,
+          'line-color': '#ccc',
+          'target-arrow-color': '#ccc',
+          'target-arrow-shape': 'triangle',
+        }
+      }
+    ],
+    layout: {
+      name: 'grid',
+      rows: 10
+    }})
+}
+
+// init cytoscape instance:
+document.addEventListener('DOMContentLoaded', initCY)
